1aefe0c111eef8f52223aede1128945b6012f4f8
Albertohdez02 <alu0101433905@ull.edu.es>
2025-05-01 12:32:06 +0000

    Actualizacion de variables de entorno

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -4,11 +4,15 @@ import {connect} from 'mongoose';
 
 // no se por que pero no me deja conectar a la base de datos en la nube
 // la uri de la base de datos en la nube esta en el archivo .env en la raiz del proyecto
-// solo hay que cambiar la url local en el connect por la de abajo
-// process.env.MONGODB_URI!
+// ES ALGO DE LA DNS DE LA ULL
+// connect(process.env.MONGODB_URI!).then(() => {
+//   console.log('Connected to the White Wolf Inn database in the cloud');
+// }).catch((error) => {
+//   console.log('Something went wrong when conecting to the database:', error);
+// });
 
 connect('mongodb://127.0.0.1:27017/white-wolf-inn').then(() => {
-  console.log('Connected to the White Wolf Inn database');
-}).catch(() => {
-  console.log('Something went wrong when conecting to the database');
+  console.log('Connected to the White Wolf Inn database locally');
+}).catch((error) => {
+  console.log('Something went wrong when conecting to the database:', error);
 });
