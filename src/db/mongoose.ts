4ff75a4d19f1389cc28ab64434c69ebaed590d48
Albertohdez02 <alu0101433905@ull.edu.es>
2025-05-01 10:25:14 +0000

    cambio de la connection string de atlas (sigue sin funcionar)

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -5,7 +5,7 @@ import {connect} from 'mongoose';
 // solo hay que cambiar la url local en el connect por la de abajo
 // process.env.MONGODB_URI!
 
-connect('mongodb://127.0.0.1:27017/white-wolf-inn').then(() => {
+connect(process.env.MONGODB_URI!).then(() => {
   console.log('Connected to the White Wolf Inn database');
 }).catch(() => {
   console.log('Something went wrong when conecting to the database');
