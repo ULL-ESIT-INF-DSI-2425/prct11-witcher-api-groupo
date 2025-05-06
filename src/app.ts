38f13f8529fc3e1aa9969e5754636289aa19ef93
marioguerra2002 <alu0101395036@ull.edu.es>
2025-05-06 00:03:47 +0000

    xd

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import "./db/mongoose.js";
 import { goodsRouter } from "./routers/goodsRouter.js";
 import { clientRouter } from './routers/clientsRouter.js';
 import { merchantsRouter } from "./routers/merchantRouter.js";
-//import { transactionsRouter } from './routers/transactionsRouter.js';
+import { transactionRouter } from "./routers/transactionRouter.js";
 import { defaultRouter } from "./routers/default.js";
 
 export const app = express();
@@ -11,5 +11,5 @@ app.use(express.json());
 app.use(goodsRouter);
 app.use(clientRouter);
 app.use(merchantsRouter);
-// app.use(transactionsRouter);
+app.use(transactionRouter);
 app.use(defaultRouter);
