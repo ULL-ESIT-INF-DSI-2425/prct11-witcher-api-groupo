308978aa9a161cdfb68594f48ebfcdba383b7314
Javier Ramos <alu0101313313@ull.edu.es>
2025-05-06 18:09:35 +0000

    arreglando variables entorno

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -11,8 +11,15 @@ import {connect} from 'mongoose';
 //   console.log('Something went wrong when conecting to the database:', error);
 // });
 
-connect('mongodb://127.0.0.1:27017/white-wolf-inn').then(() => {
-  console.log('Connected to the White Wolf Inn database locally');
-}).catch((error) => {
-  console.log('Something went wrong when conecting to the database:', error);
-});
+// connect('mongodb://127.0.0.1:27017/white-wolf-inn').then(() => {
+//   console.log('Connected to the White Wolf Inn database locally');
+// }).catch((error) => {
+//   console.log('Something went wrong when conecting to the database:', error);
+// });
+
+try {
+  await connect(process.env.MONGODB_URL!);
+  console.log("Connection to MongoDB server established");
+} catch (error) {
+  console.log(error);
+}
