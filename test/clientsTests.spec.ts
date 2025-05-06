733a0aef37c07dc7fa24c1030465c48b339c4a77
Javier Ramos <alu0101313313@ull.edu.es>
2025-05-06 18:02:10 +0000

    workflows

--- test/clientsTests.spec.ts
+++ test/clientsTests.spec.ts
@@ -1,27 +1,41 @@
-import { describe, it, beforeAll } from 'vitest';
+import { describe, it, beforeAll, expect } from 'vitest';
 import request from 'supertest';
 import { app } from '../src/app.js';
 import { Client } from '../src/models/clientModel.js';
 
+const firstClient = {
+  id: 9,
+  name: 'ClientePrueba',
+  race: 'halveling',
+  location: 'redania',
+}
+
 beforeAll(async () => {
   await Client.deleteMany();
+  await new Client(firstClient).save();
 });
 
 describe('Client API', () => {
   it('Should successfully create a new client', async () => {
-    await request(app)
-      .post('/hunters')
-      .send({
-        id: 3,
-        name: 'Geralt of Rivia',
-        race: 'human',
-        location: "redania"
-      }).expect(201);
+    const response = await request(app)
+    .post('/hunters')
+    .send({
+      id: 3,
+      name: 'Geralt of Rivia',
+      race: 'human',
+      location: "redania"
+    }).expect(201);
+
+    expect(response.body).to.include({
+      id: 3,
+      name: 'Geralt of Rivia',
+      race: 'human',
+      location: "redania"
+    });
   });
 
   it('Should send an error while create a new client', async () => {
-    await request(app)
-      .post('/hunters')
+    await request(app).post('/hunters')
       .send({
         name: 'Geralt of Rivia',
         location: "redania"
@@ -29,19 +43,23 @@ describe('Client API', () => {
   });
 
   it('Should find an existing client', async () => {
-    await request(app).get('/hunters?name=Geralt of Rivia').expect(201);
+    await request(app).get('/hunters?name=Geralt of Rivia').expect(202);
   });
 
   it('Should not find an existing client', async () => {
     await request(app).get('/hunters?name=Garalt').expect(404);
   });
 
-  // it('Should find an existing client by id', async () => {
-  //   await request(app).get('/hunters/68194df25ce1f68d22aa0c63').expect(201);
-  // });
+  it('Should send an error while find an existing client', async () => {
+    await request(app).get('/hunters?name= ').expect(500);
+  });
+
+  it('Should find an existing client by id', async () => {
+    await request(app).get('/hunters/6819e6be2fdd147e408e7b80').expect(202);
+  });
 
   it('Should not find an existing client by id', async () => {
-    await request(app).get('/hunters/68194df25ce1f68d22aa0d63').expect(404);
+    await request(app).get('/hunters/6819e6be2fdd147e408e7b80').expect(404);
   });
 
   it('Should successfully modify an existing client', async () => {
