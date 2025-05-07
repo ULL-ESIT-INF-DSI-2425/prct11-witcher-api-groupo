77bac596b373b9c4d4c3cb4e424c6b5a8f066d71
Albertohdez02 <alu0101433905@ull.edu.es>
2025-05-07 23:09:20 +0000

    Mejora de seguridad para Sonarqube

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,8 @@ import { transactionRouter } from "./routers/transactionRouter.js";
 import { defaultRouter } from "./routers/default.js";
 
 export const app = express();
+// Disable the "x-powered-by" header to prevent information leakage
+app.disable("x-powered-by")
 app.use(express.json());
 app.use(goodsRouter);
 app.use(clientRouter);
